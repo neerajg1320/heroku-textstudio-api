3f9ac5846472858ca89bf3bc949b5c4e23275bda
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-03-24 17:50:19 +0530

    Modified heroku.cmd

--- heroku.cmd
+++ heroku.cmd
@@ -1,2 +1,3 @@
 # https://stackoverflow.com/questions/54327005/installing-pdftotext-library-on-heroku
 heroku buildpacks:add  -a textstudio-api heroku-community/apt
+heroku buildpacks:add  -a textstudio-api heroku/python
